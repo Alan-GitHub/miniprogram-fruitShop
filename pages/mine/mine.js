c790bbee54150fb1aaaeb13959adcdfca0907634
Alan-GitHub <584840653@qq.com>
2019-04-17 21:12:20 +0800

    我的页面菜单项条目从后台拉取展示

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -13,7 +13,7 @@ Page({
     userInfo: {},
     hasUserInfo:false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    mine_menuitem: ["消费明细", "积分明细", "积分兑换"]
+    mine_menuitem: [] //这个字段的值从后台拉取
   },
 
   /**
@@ -29,6 +29,30 @@ Page({
         }
       })
     }
+  },
+
+  onLoad: function () {
+    var db = wx.cloud.database({
+      env: 'alan-test-eef1c6'  //必须是环境id
+    })
+
+    //这段代码采用回调方式，但是没有达到预期效果，从后台拉取数据后并不能立即显示在页面上
+    //而下方的promise风格的却没有这个问题
+    // db.collection("mine_colle").get({
+    //   success(res) {
+    //     var that = this
+    //     console.log(res.data)
+    //     that.setData({
+    //       mine_menuitem: res.data[0].mine_menuitem
+    //     })
+    //   }
+    // })
+
+    db.collection("mine_colle").get().then(res => {
+      this.setData({
+        mine_menuitem: res.data[0].mine_menuitem
+      })
+    })
   },
 
   getUserInfo: function (e) {
